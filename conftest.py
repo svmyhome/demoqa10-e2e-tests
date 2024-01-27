524ae9bd8b6ae2ed58f67df89d4530337552a950
vladimir <svmyhome@mail.ru>
2024-01-27 21:18:15 +0300

    Add homework for lesson 6. Python basics

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selene import browser
-
-@pytest.fixture
-def browser_management():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1300
-    browser.config.window_height = 2000
