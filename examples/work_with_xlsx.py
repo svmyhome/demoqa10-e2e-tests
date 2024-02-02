dddd77056a724646626be47f4b9f3eabf94a924c
vladimir <svmyhome@mail.ru>
2024-02-02 23:20:06 +0300

    Add tests

--- examples/work_with_xlsx.py
+++
@@ -1,9 +0,0 @@
-import os
-from openpyxl import load_workbook
-
-os.chdir('../resources')
-path = os.getcwd()
-workbook = load_workbook(os.path.join(path, "calendare.xlsx"))
-sheet = workbook.active
-
-print(sheet.cell(3, 3).value)
