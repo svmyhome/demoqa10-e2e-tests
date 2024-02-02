9d61098730c2f82eb19821702d0bd1f30e9b2aed
vladimir <svmyhome@mail.ru>
2024-02-02 19:44:58 +0300

    Add extract files

--- homework.py
+++ homework.py
@@ -6,7 +6,6 @@ from zipfile import ZipFile
 def create_zip(file_archive):
     path = os.path.join(os.getcwd(), 'resources')
     list_dir = os.listdir(path)
-    print(list_dir)
     with ZipFile(file_archive, 'w', compression=zipfile.ZIP_DEFLATED) as zf:
         for file_name in list_dir:
             add_file = os.path.join(path, file_name)
@@ -19,5 +18,25 @@ def add_file_to_zip(name_archive, name_file):
         zf.write(name_file)
 
 
+def list_files_to_zip(name_archive):
+    with ZipFile(name_archive, 'a') as zf:
+        print(zf.namelist())
+
+
+def extract_all_files(name_archive):
+    extract_dir = 'extract_dir'
+    with ZipFile(name_archive) as zf:
+        zf.extractall(extract_dir)
+
+
+def extract_file(name_archive, name_file):
+    extract_dir = 'extract_dir_one'
+    with ZipFile(name_archive) as zf:
+        zf.extract(name_file, extract_dir)
+
+
 create_zip('test1.zip')
 add_file_to_zip('test1.zip', 'README.md')
+list_files_to_zip('test1.zip')
+extract_all_files('test1.zip')
+extract_file('test1.zip', 'README.rst')
