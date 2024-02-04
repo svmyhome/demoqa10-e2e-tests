16906a2f908b3dffed88ea2d161b9b01e131b518
vladimir <svmyhome@mail.ru>
2024-02-04 20:37:05 +0300

    Lesson 8: add simple test
    This is simple test without refactoring

---
+++ tests/simple.py
@@ -0,0 +1,16 @@
+import csv
+
+
+def test_workers_are_adults():
+    with open('../resources/people.csv') as csv_file:
+        users = csv.DictReader(csv_file, delimiter=';')
+        print()
+        workers = [user for user in users if user['status'] == 'worker']
+        # for user in users:
+        #     if user['status'] == 'worker':
+        #         workers.append(user)
+
+    for worker in workers:
+        assert (
+            int(worker['age']) >= 18
+        ), f'The worker {worker["name"]} have {worker["age"]} year'
