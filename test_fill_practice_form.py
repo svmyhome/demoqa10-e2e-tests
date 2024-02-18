4f9039827d39cfc458259969b93d1a287a56145c
Vladimir Sarychev <svmyhome@mail.ru>
2024-02-18 11:14:13 +0300

    Lesson 5 revision: refactoring test

--- test_fill_practice_form.py
+++ test_fill_practice_form.py
@@ -3,7 +3,7 @@ import os
 from selene import browser, be, have, command
 
 YEAR = '2019'
-MONTH = '7'
+MONTH = '8'
 DAY = '01'
 
 
@@ -14,7 +14,7 @@ def test_fill_practice_form_without_revision(browser_management):
     browser.element('#lastName').should(be.blank).type('Petrov')
     browser.element('#userEmail').should(be.blank).type('qaz@mail.ru')
 
-    browser.element('[for=gender-radio-1]').click()
+    browser.all('[for^=gender-radio]').element_by(have.exact_text('Male')).click()
 
     browser.element('#userNumber').should(be.blank).type('0123456789')
 
@@ -23,23 +23,24 @@ def test_fill_practice_form_without_revision(browser_management):
     browser.element(f"[value='{YEAR}']").click()
     browser.element(".react-datepicker__month-select").click()
     browser.element(f"[value='{MONTH}']").click()
-    browser.element(f".react-datepicker__day--0{DAY}[tabindex='-1']").click()
+    browser.element(f".react-datepicker__day--0{DAY}:not(.react-datepicker__day--outside-month)").click()
 
     browser.element("#subjectsContainer").click().element('#subjectsInput').type('p')
 
-    browser.element("#react-select-2-option-0").click()
-    browser.element("[for='hobbies-checkbox-1']").click()
-    browser.element("[for='hobbies-checkbox-2']").click()
-    browser.element("[for='hobbies-checkbox-3']").click()
+    browser.all("[id^=react-select-2-option]").element_by(have.exact_text('Physics')).click()
+
+    browser.all("[for^='hobbies-checkbox']").element_by(have.exact_text('Sports')).click()
+    browser.all("[for^='hobbies-checkbox']").element_by(have.exact_text('Reading')).click()
+    browser.all("[for^='hobbies-checkbox']").element_by(have.exact_text('Music')).click()
 
     browser.element('#uploadPicture').send_keys(os.path.abspath('README.md'))
 
     browser.element('#currentAddress').should(be.blank).type('1234 casc csdc 56789')
 
     browser.element('#state').click()
-    browser.element('#react-select-3-option-2').click()
+    browser.all("[id^='react-select-3-option']").element_by(have.exact_text('Uttar Pradesh')).click()
     browser.element('#city').click()
-    browser.element('#react-select-4-option-0').click()
+    browser.all('[id^=react-select-4-option]').element_by(have.exact_text('Lucknow')).click()
     browser.element('#submit').click()
 
     browser.all('.table-responsive tbody>tr').should(
@@ -48,98 +49,11 @@ def test_fill_practice_form_without_revision(browser_management):
             'Student Email qaz@mail.ru',
             'Gender Male',
             'Mobile 0123456789',
-            'Date of Birth 01 August,2019',
+            'Date of Birth 01 September,2019',
             'Subjects Physics',
             'Hobbies Sports, Reading, Music',
             'Picture README.md',
             'Address 1234 casc csdc 56789',
-            'State and City Haryana Karnal',
+            'State and City Uttar Pradesh Lucknow',
         )
     )
-
-
-def test_fill_practice_form_with_revision_1(browser_management):
-    browser.open('/automation-practice-form')
-
-    browser.element('#firstName').should(be.blank).type('Ivan')
-    browser.element('#lastName').should(be.blank).type('Petrov')
-    browser.element('#userEmail').should(be.blank).type('qaz@mail.ru')
-
-    browser.all('[for^=gender-radio]').element_by(have.exact_text('Male')).click()
-    # browser.all('[for^=gender-radio]').element_by(have.text('Male')).click()
-    # browser.all('[name=gender]').element_by(have.value('Female')).element('..').click()
-    # browser.element('[name=gender][value=Male]+label').click()
-    # browser.all('[name=gender]').element_by(have.value('Male')).element('./following-sibling::*').click()
-
-    browser.element('#userNumber').should(be.blank).type('0123456789')
-
-    browser.element('#dateOfBirthInput').click()
-    browser.element(".react-datepicker__year-select").type('2012')
-    # browser.element(".react-datepicker__year-select").click()
-    # browser.element("[value='2019']").click()
-    browser.element(".react-datepicker__month-select").click()
-    # browser.element('[class~="react-datepicker__month-select"]').click()
-    # browser.element("[value='7']").click()
-    browser.element('[class~="react-datepicker__month-select"]').all('option')[
-        4
-    ].click()
-    browser.element(f".react-datepicker__day--001[tabindex='-1']").click()
-
-    browser.element("#subjectsContainer").click().element('#subjectsInput').type('p')
-
-    browser.element("#react-select-2-option-0").click()
-    browser.element("[for='hobbies-checkbox-1']").click()
-    browser.element("[for='hobbies-checkbox-2']").click()
-    browser.element("[for='hobbies-checkbox-3']").click()
-
-    browser.element('#uploadPicture').send_keys(os.path.abspath('README.md'))
-
-    browser.element('#currentAddress').perform(command.js.scroll_into_view)
-    browser.element('#currentAddress').perform(
-        command.js.set_value('1234 casc 33e csdc 56789')
-    )
-    # browser.element('#currentAddress').should(be.blank).type('1234 casc csdc 56789')
-
-    browser.element('#state').click()
-    browser.element('#react-select-3-option-2').click()
-    browser.element('#city').click()
-    browser.element('#react-select-4-option-0').click()
-    browser.element('#submit').click()
-
-    # browser.all('.table tbody>tr').should(
-    #     have.exact_texts(
-    #         'Student Name Ivan Petrov',
-    #         'Student Email qaz@mail.ru',
-    #         'Gender Male',
-    #         'Mobile 0123456789',
-    #         'Date of Birth 01 August,2019',
-    #         'Subjects Physics',
-    #         'Hobbies Sports, Reading, Music',
-    #         'Picture README.md',
-    #         'Address 1234 casc csdc 56789',
-    #         'State and City Haryana Karnal',
-    #     )
-    # )
-    list_data = [
-        'Student Name',
-        'Ivan Petrov',
-        'Student Email',
-        'qaz@mail.ru',
-        'Gender',
-        'Male',
-        'Mobile',
-        '0123456789',
-        'Date of Birth',
-        '01 August,2019',
-        'Subjects',
-        'Physics',
-        'Hobbies',
-        'Sports, Reading, Music',
-        'Picture',
-        'README.md',
-        'Address',
-        '1234 casc csdc 56789',
-        'State and City',
-        'Haryana Karnal',
-    ]
-    browser.element('.table').all('td').should(have.texts())
