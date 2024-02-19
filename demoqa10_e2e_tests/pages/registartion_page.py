c1477f7049c14e15eebce0bcf1e2f2ab28e1caa2
Vladimir Sarychev <svmyhome@mail.ru>
2024-02-19 22:33:08 +0300

    Lesson 10: part one hi-level-step-objects complit work test

--- demoqa10_e2e_tests/pages/registartion_page.py
+++ demoqa10_e2e_tests/pages/registartion_page.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 from enum import Enum
 from demoqa10_e2e_tests.data.user import User
 from selene import browser, be, have
@@ -26,26 +25,6 @@ class RegistrationPage:
     def open_page(self):
         browser.open('/automation-practice-form')
         return self
-
-    # def fill_first_name(self, value):
-    #     browser.element('#firstName').should(be.blank).type(value)
-    #     return self
-    #
-    # def fill_last_name(self, value):
-    #     browser.element('#lastName').should(be.blank).type(value)
-    #     return self
-    #
-    # def fill_email(self, value):
-    #     browser.element('#userEmail').should(be.blank).type(value)
-    #     return self
-
-    # def gender_choose(self, value):
-    #     browser.all('[for^=gender-radio]').element_by(have.exact_text(value)).click()
-    #     return self
-
-    # def fill_mobile(self, value):
-    #     browser.element('#userNumber').should(be.blank).type(value)
-    #     return self
 
     def fill_date_of_birth(self, birthday):
         if birthday.day < 10:
@@ -79,146 +58,32 @@ class RegistrationPage:
             ).click()
         return self
 
-    # def select_picture(self, path):
-    #     browser.element('#uploadPicture').send_keys(os.path.abspath(path))
-    #     return self
-
-    # def fill_current_address(self, adress):
-    #     browser.element('#currentAddress').should(be.blank).type(adress)
-    #     return self
-
-    # def state_choose(self, value):
-    #     self.state.click()
-    #     browser.all("[id^='react-select-3-option']").element_by(
-    #         have.exact_text(value)
-    #     ).click()
-    #     return self
-    #
-    # def city_choose(self, value):
-    #     self.city.click()
-    #     browser.all('[id^=react-select-4-option]').element_by(
-    #         have.exact_text(value)
-    #     ).click()
-    #     return self
-
     def submit(self):
         browser.element('#submit').click()
         return self
 
-    def assert_fill_form(
-        self,
-        name,
-        email,
-        gender,
-        mobile,
-        birthday,
-        subjects,
-        hobbies,
-        picture,
-        address,
-        state_city,
-    ):
-        browser.element('.table-responsive').all('tbody>tr').should(
-            have.exact_texts(
-                name,
-                email,
-                gender,
-                mobile,
-                birthday,
-                subjects,
-                hobbies,
-                picture,
-                address,
-                state_city,
-            )
-        )
-
     def get_form_table_cells(self):
         return browser.element('.table-responsive').all('td')
 
-    def register(
-        self,
-        first_name,
-        last_name,
-        email,
-        gender,
-        mobile,
-        birthday,
-        subjects,
-        hobbies: list,
-        picture,
-        address,
-        state,
-        city,
-    ):
-        self.first_name.should(be.blank).type(first_name)
-        self.last_name.should(be.blank).type(last_name)
-        self.email.should(be.blank).type(email)
-        self.gender.element_by(have.exact_text(gender)).click()
-        self.mobile.should(be.blank).type(mobile)
-        self.fill_date_of_birth(birthday)
-        self.fill_subjects(subjects)
-        self.hobbies_choose(hobbies)
-        self.picture.send_keys(os.path.abspath(f'../{picture}'))
-        self.current_address.should(be.blank).type(address)
+    def register(self, user: User):
+        self.first_name.should(be.blank).type(user.first_name)
+        self.last_name.should(be.blank).type(user.last_name)
+        self.email.should(be.blank).type(user.email)
+        self.gender.element_by(have.exact_text(user.gender)).click()
+        self.mobile.should(be.blank).type(user.mobile)
+        self.fill_date_of_birth(user.date_of_birth)
+        self.fill_subjects(user.subjects)
+        self.hobbies_choose(user.hobbies)
+        self.picture.send_keys(os.path.abspath(f'../{user.picture}'))
+        self.current_address.should(be.blank).type(user.address)
         self.state.click()
         browser.all("[id^='react-select-3-option']").element_by(
-            have.exact_text(state)
+            have.exact_text(user.state)
         ).click()
 
         self.city.click()
         browser.all('[id^=react-select-4-option]').element_by(
-            have.exact_text(city)
+            have.exact_text(user.city)
         ).click()
 
         return self
-
-
-if __name__ == '__main__':
-    user = User(
-        'Ivan',
-        'Petrov',
-        'qaz@mail.ru',
-        'Male',
-        '0123456789',
-        datetime.date(2019, 8, 1),
-        ('p', 'Physics'),
-        [Hobbies.sport, Hobbies.reading, Hobbies.music],
-        'README.md',
-        'SPB, lenina 10',
-        'Uttar Pradesh',
-        'Lucknow',
-    )
-    print(user)
-    # class Weapon(Enum):
-    #     SWORD = 1
-    #     BOW = 2
-    #     DAGGER = 3
-    #     CLUB = 4
-    #
-    #
-    # ranged_weapon = Weapon.BOW
-    # print(ranged_weapon)
-    #
-    # if ranged_weapon == Weapon.BOW:
-    #     print("It's a bow")
-    #
-    # print(list(Weapon))
-    #
-    # weapon = Weapon.SWORD
-    #
-    # print(weapon)
-    # print(isinstance(weapon, Weapon))
-    # print(type(weapon))
-    # print(repr(weapon))
-    #
-    # print(Weapon['SWORD'])
-    # print(Weapon(1))
-
-    # birthday = datetime.date(2019, 8, 1)
-    # print(birthday)
-    # if birthday.day < 10:
-    #     day = '0' + str(birthday.day)
-    # else:
-    #     day = str(birthday.day)
-    # print(day)
--- tests/test_fill_practice_form_page_object.py
+++ tests/test_fill_practice_form_page_object.py
@@ -1,8 +1,7 @@
 import datetime
-import os
 from demoqa10_e2e_tests.data.user import User
 
-from selene import browser, be, have
+from selene import have
 
 from demoqa10_e2e_tests.pages.registartion_page import RegistrationPage, Hobbies
 
@@ -11,110 +10,8 @@ MONTH = 8
 DAY = 1
 
 
-# def test_fill_practice_form_with_revision(browser_management):
-#     user_registration = RegistrationPage()
-#
-#     user_registration.open_page()
-#
-#     user_registration.fill_first_name('Ivan')
-#     user_registration.fill_last_name('Petrov')
-#     user_registration.fill_email('qaz@mail.ru')
-#
-#     user_registration.gender_choose('Male')
-#
-#     user_registration.fill_mobile('0123456789')
-#
-#     user_registration.fill_date_of_birth(YEAR, MONTH, DAY)
-#
-#     user_registration.fill_subjects('Physics')
-#
-#     user_registration.hobbies_choose('Sports', 'Reading', 'Music')
-#
-#     user_registration.select_picture('../README.md')
-#
-#     user_registration.fill_current_address('SPB, lenina 10')
-#
-#     user_registration.state_choose('Uttar Pradesh')
-#
-#     user_registration.city_choose('Lucknow')
-#
-#     user_registration.submit()
-#
-#     user_registration.assert_fill_form(
-#         'Student Name Ivan Petrov',
-#         'Student Email qaz@mail.ru',
-#         'Gender Male',
-#         'Mobile 0123456789',
-#         'Date of Birth 01 September,2019',
-#         'Subjects Physics',
-#         'Hobbies Sports, Reading, Music',
-#         'Picture README.md',
-#         'Address SPB, lenina 10',
-#         'State and City Uttar Pradesh Lucknow',
-#     )
-#
-#
-# def test_fill_practice_form_with_revision_table(browser_management):
-#     user_registration = RegistrationPage()
-#
-#     user_registration.open_page().fill_first_name('Ivan').fill_last_name(
-#         'Petrov'
-#     ).fill_email('qaz@mail.ru')
-#
-#     user_registration.gender_choose('Male')
-#
-#     user_registration.fill_mobile('0123456789')
-#
-#     user_registration.fill_date_of_birth(YEAR, MONTH, DAY)
-#
-#     user_registration.fill_subjects('Physics')
-#
-#     user_registration.hobbies_choose('Sports', 'Reading', 'Music')
-#
-#     user_registration.select_picture('../README.md')
-#
-#     user_registration.fill_current_address('SPB, lenina 10')
-#
-#     user_registration.state_choose('Uttar Pradesh').city_choose('Lucknow')
-#
-#     user_registration.submit()
-#
-#     user_registration.get_form_table_cells().should(
-#         have.exact_texts(
-#             ('Student Name', 'Ivan Petrov'),
-#             ('Student Email', 'qaz@mail.ru'),
-#             ('Gender', 'Male'),
-#             ('Mobile', '0123456789'),
-#             ('Date of Birth', '01 September,2019'),
-#             ('Subjects', 'Physics'),
-#             ('Hobbies', 'Sports, Reading, Music'),
-#             ('Picture', 'README.md'),
-#             ('Address', 'SPB, lenina 10'),
-#             ('State and City', 'Uttar Pradesh Lucknow'),
-#         )
-#     )
-
-
 def test_fill_practice_form_with_revision_table_hi_level(browser_management):
     user = User(
-        'Ivan',
-        'Petrov',
-        'qaz@mail.ru',
-        'Male',
-        '0123456789',
-        datetime.date(2019, 8, 1),
-        ('p', 'Physics'),
-        [Hobbies.sport, Hobbies.reading, Hobbies.music],
-        'README.md',
-        'SPB, lenina 10',
-        'Uttar Pradesh',
-        'Lucknow',
-    )
-    user_registration = RegistrationPage()
-
-    user_registration.open_page()
-
-    user_registration.register(
         'Ivan',
         'Petrov',
         'qaz@mail.ru',
@@ -128,6 +25,11 @@ def test_fill_practice_form_with_revision_table_hi_level(browser_management):
         'Uttar Pradesh',
         'Lucknow',
     )
+    user_registration = RegistrationPage()
+
+    user_registration.open_page()
+
+    user_registration.register(user)
 
     user_registration.submit()
 
