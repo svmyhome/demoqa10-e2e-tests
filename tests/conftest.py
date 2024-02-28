36d41f658b8899a5184ec916975fa0cfd532a3ab
Vladimir Sarychev <svmyhome@mail.ru>
2024-02-28 15:34:54 +0300

    Lesson 12: Jenkins delete some files

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,20 +10,20 @@ from selene import Browser, Config
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {"enableVNC": True, "enableVideo": True},
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options,
-    )
-
-    browser = Browser(Config(driver))
+def browser_management(request):
+    # options = Options()
+    # selenoid_capabilities = {
+    #     "browserName": "chrome",
+    #     "browserVersion": "100.0",
+    #     "selenoid:options": {"enableVNC": True, "enableVideo": True},
+    # }
+    # options.capabilities.update(selenoid_capabilities)
+    # driver = webdriver.Remote(
+    #     command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+    #     options=options,
+    # )
+    #
+    # browser = Browser(Config(driver))
     dotenv.load_dotenv()
     with allure.step('Driver configuration'):
         browser.config.base_url = project.config.base_url
