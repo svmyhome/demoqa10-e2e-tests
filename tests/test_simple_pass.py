d4b32071ff91c597d838c41fd91a0532d84965b6
Vladimir Sarychev <svmyhome@mail.ru>
2024-02-28 12:45:12 +0300

    Lesson 12: Jenkins add allure

--- tests/test_simple_pass.py
+++ tests/test_simple_pass.py
@@ -1,8 +1,20 @@
 import pytest
+import allure
+from allure_commons.types import Severity
 
 
+@allure.tag('PASS', 'Проверка внутри Page object')
+@allure.feature('Text Box 1')
+@allure.story('Register the user')
+@allure.label('OWNER', 'Vladimir')
+@allure.severity(Severity.BLOCKER)
+@allure.link('https://demoqa.com', name='PASS')
 def test_pass_1():
-    pass
+    with allure.step('Open page'):
+        print()
+
+    with allure.step('Submit form'):
+        print()
 
 
 def test_pass_2():
