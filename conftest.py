06940c35e32cd11ec232df84670ef534eff416e5
Vladimir Sarychev <svmyhome@mail.ru>
2024-02-28 20:24:16 +0300

    Lesson 12: Jenkins one file add

--- conftest.py
+++ conftest.py
@@ -11,12 +11,15 @@ def setup_browser(request):
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
-        "selenoid:options": {"enableVNC": True, "enableVideo": True},
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options,
+        options=options
     )
 
     browser = Browser(Config(driver))
