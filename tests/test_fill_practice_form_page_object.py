2dbacb969f375b27ef2c840f80e9f0e08f4ced71
Vladimir Sarychev <svmyhome@mail.ru>
2024-02-28 13:12:58 +0300

    Lesson 12: Jenkins add allure description

--- tests/test_fill_practice_form_page_object.py
+++ tests/test_fill_practice_form_page_object.py
@@ -1,7 +1,16 @@
+import allure
+from allure_commons.types import Severity
+
 from demoqa10_e2e_tests.pages.registration_page import RegistrationPage
 from demoqa10_e2e_tests.test_data import users
 
 
+@allure.tag('DemoQA', 'Проверка внутри Page object')
+@allure.feature('Registration Page')
+@allure.story('Register the user')
+@allure.label('OWNER', 'Vladimir')
+@allure.severity(Severity.BLOCKER)
+@allure.link('https://demoqa.com', name='Registration page')
 def test_fill_practice_form_with_revision_table_hi_level():
     worker = users.advanced_user
 
