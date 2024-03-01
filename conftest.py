46482e467bb83866d9fe22685d0e694d4735131b
Vladimir Sarychev <svmyhome@mail.ru>
2024-03-01 15:03:28 +0300

    Lesson 12: Jenkins add several tests

--- conftest.py
+++ conftest.py
@@ -33,3 +33,18 @@ def setup_browser(request):
     attach.add_video(browser)
 
     browser.quit()
+
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_management():
+#     dotenv.load_dotenv()
+#     with allure.step('Driver configuration'):
+#         browser.config.base_url = project.config.base_url
+#         browser.config.driver_name = project.config.driver_name
+#         browser.config.hold_driver_at_exit = project.config.hold_driver_at_exit
+#         browser.config.window_width = project.config.window_width
+#         browser.config.window_height = project.config.window_height
+#         browser.config.timeout = project.config.timeout
+#
+#     yield
+#     with allure.step('Close driver'):
+#         browser.quit()
--- demoqa10_e2e_tests/utils/resource.py
+++ demoqa10_e2e_tests/utils/resource.py
@@ -1,8 +1,14 @@
-import demoqa10_e2e_tests
 from pathlib import Path
 
 
+def path(file_name):
+    return str(Path(__file__).parent.parent.parent.joinpath(f'resources/{file_name}'))
+
+
 def relative_from_root(path: str):
+    import demoqa10_e2e_tests
+    from pathlib import Path
+
     return (
         Path(demoqa10_e2e_tests.__file__)
         .parent.parent.joinpath(path)
