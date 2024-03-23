b8b4f5abd5685cc912002b9f8043d0c42517efbe
Vladimir Sarychev <svmyhome@mail.ru>
2024-03-23 10:47:34 +0300

    lesson_19-RestApi-part1 corrections test

--- tests/reqres/test_delete_user.py
+++ tests/reqres/test_delete_user.py
@@ -6,5 +6,5 @@ from requests import Response
 @pytest.mark.positive
 def test_delete_user(base_url):
     response: Response = requests.delete(f'{base_url}users/2')
-    print(response.text)
     assert response.status_code == 204
+    assert response.text == ""
--- tests/reqres/test_list_users.py
+++ tests/reqres/test_list_users.py
@@ -4,7 +4,7 @@ import pytest
 from requests import Response
 from jsonschema import validate
 from demoqa10_e2e_tests.utils.resource import relative_from_root
-from tests.schemas import reqres_schemas
+from schemas import reqres_schemas
 
 
 @pytest.mark.positive
@@ -12,23 +12,11 @@ def test_list_users_get(base_url):
     schema = reqres_schemas.list_users
 
     response: Response = requests.get(f'{base_url}users', params={"page": 2})
-    print(response.text)
 
     assert response.status_code == 200
     validate(response.json(), schema)
-
-
-@pytest.mark.positive
-def test_list_users_with_json_get(base_url):
-    file_path = relative_from_root('tests/schemas/list_users.json')
-    with open(file_path) as file:
-        file = json.load(file)
-
-    response: Response = requests.get(f'{base_url}users', params={"page": 2})
-    print(response.text)
-
-    assert response.status_code == 200
-    validate(response.json(), file)
+    assert response.json()['data'][0]['id'] == 7
+    assert response.json()['data'][0]['first_name'] == 'Michael'
 
 
 @pytest.mark.negative
@@ -36,7 +24,7 @@ def test_list_user_not_found_get(base_url):
     schema = reqres_schemas.user_not_found
 
     response: Response = requests.get(f'{base_url}users/2322')
-    print(response.text)
 
     assert response.status_code == 404
     validate(response.json(), schema)
+    assert response.json() == {}
