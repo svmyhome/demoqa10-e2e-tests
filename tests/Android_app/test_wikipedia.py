3bd931b06143f27fd1d256665de630941279882d
Vladimir Sarychev <svmyhome@mail.ru>
2024-04-06 10:27:55 +0300

    lesson_21: Mobile automation add part of second test for android

--- tests/Android_app/test_wikipedia.py
+++ tests/Android_app/test_wikipedia.py
@@ -8,16 +8,34 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-def test_search():
+def test_search_appium():
 
     with allure.step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-
 
     browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
         "Appium"
     )
     with allure.step('Verify content found'):
-        results = browser.all((AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title"))
+        results = browser.all(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
+        )
+        for i in results:
+            print(i)
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+
+
+def test_nothing_found():
+    with allure.step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+
+    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
+        "1234567890qaz"
+    )
+
+    with allure.step('Verify content not found'):
+        results = browser.all(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
+        )
+        results.first.should(have.text('No results'))
