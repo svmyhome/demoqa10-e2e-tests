d4109f33bf4f8d26552a0a7116149a60611c5053
Vladimir Sarychev <svmyhome@mail.ru>
2024-04-15 09:24:59 +0300

    lesson_21: Mobile automation add test for android

--- tests/Android_app/test_wikipedia.py
+++ tests/Android_app/test_wikipedia.py
@@ -5,13 +5,14 @@ from selene import browser, have
 
 def test_search_appium():
 
-    with allure.step('Type search'):
+    with allure.step('Click to search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
 
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
-        "Appium"
-    )
-    with allure.step('Verify content found'):
+    with allure.step('Type the text'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
+            "Appium"
+        )
+    with allure.step('Assert text found'):
         results = browser.all(
             (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
         )
@@ -21,67 +22,55 @@ def test_search_appium():
 
 def test_search_python():
 
-    with allure.step('Type search'):
+    with allure.step('Click to search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
-        "Python"
-    )
-    with allure.step('Verify content found'):
+    with allure.step('Type the text'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
+            "Python"
+        )
+    with allure.step('Assert text found'):
         results = browser.all(
             (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
         )
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Python'))
 
-def test_search_1234567890():
 
-    with allure.step('Type search'):
+def test_text_not_found_1():
+    with allure.step('Click to search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
-        "1234567890"
-    )
-    with allure.step('Verify content found'):
-        results = browser.all(
-            (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
+    with allure.step('Type the text'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
+            "1234567890qaz"
         )
-        results.should(have.size_greater_than(0))
-        results.first.should(have.text('1234567890'))
-
-
-def test_nothing_found_1():
-    with allure.step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
-        "1234567890qaz"
-    )
-
-    with allure.step('Verify content not found'):
+    with allure.step('Assert text not found'):
         results = browser.all((AppiumBy.ID, "org.wikipedia.alpha:id/results_text"))
         results.should(have.size(0))
 
-def test_nothing_found_2():
-    with allure.step('Type search'):
+
+def test_text_not_found_2():
+    with allure.step('Click to search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+    with allure.step('Type the text'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
+            "1234567890qaz"
+        )
 
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
-        "1234567890qaz"
-    )
+    with allure.step('Assert text not found'):
+        results = browser.element(
+            (AppiumBy.ID, "org.wikipedia.alpha:id/search_empty_text")
+        )
+        results.should(have.text('No results found'))
 
-    with allure.step('Verify content not found'):
-        results = browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/results_text"))
-        results.should(have.size(0))
 
-def test_nothing_found_3():
-    with allure.step('Type search'):
+def test_text_not_found_3():
+    with allure.step('Click to search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+    with allure.step('Type the text'):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
+            "1234567890qaz"
+        )
 
-    browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(
-        "1234567890qaz"
-    )
-
-    with allure.step('Verify content not found'):
-        results = browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/results_text"))
-        results.should(have.text('No results'))
+    with allure.step('Assert text not found'):
+        results = browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView"))
+        results.second.should(have.text('No results found'))
