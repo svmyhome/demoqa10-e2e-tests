ff5431b2750eeca94f85ddc351a2b5d21f374e1a
Vladimir Sarychev <svmyhome@mail.ru>
2024-05-10 19:37:39 +0300

    remove test android

--- project.py
+++ project.py
@@ -3,21 +3,25 @@ from typing import Literal
 from pydantic_settings import BaseSettings
 
 import config
+from demoqa10_e2e_tests.utils import resource
 
 
 class Settings(BaseSettings):
-    platform_name: Literal['android', 'ios', 'local'] = 'android'
+    platform_name: Literal['android', 'ios', 'local'] = config.platform_name
     app_path: str = config.app_path
     base_url: str = config.base_url
     time_out: float = config.time_out
     # android
+    udid: str = config.udid
     android_project_name: str = "Android wikipedia test"
-    android_device_name: str = config.android_device_name#"Google Pixel 4"#"Google Pixel 3"
-    android_platform_version: str = config.android_platform_version#"9.0"
+    android_device_name: str = config.android_device_name
+    android_platform_version: str = config.android_platform_version
     # ios
     ios_project_name: str = "IOS wikipedia test"
     ios_device_name: str = config.ios_device_name
     ios_platform_version: str = config.ios_platform_version
 
+    appWaitActivity: str = config.appWaitActivity
 
-settings = Settings()
+
+settings = Settings(_env_file=resource.relative_from_root(f'.env.{Settings().platform_name}'))
--- tests/conftest.py
+++ tests/conftest.py
@@ -12,26 +12,50 @@ from demoqa10_e2e_tests.utils.allure_attach import (
     attach_page_source,
     attach_video,
 )
+from demoqa10_e2e_tests.utils.resource import relative_from_root
 
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
     if project.settings.platform_name == 'android':
-        options = UiAutomator2Options().load_capabilities(
-            {
-                "platformName": project.settings.platform_name,
-                "platformVersion": project.settings.android_platform_version,
-                "deviceName": project.settings.android_device_name,
-                "app": project.settings.app_path,
-                'bstack:options': {
+        options = UiAutomator2Options()
+
+        options.set_capability("platformName", project.settings.platform_name)
+        if not config.runs_on_bstack:
+            options.set_capability('udid', project.settings.udid)
+        if project.settings.android_device_name:
+            options.set_capability("deviceName", project.settings.android_device_name)
+
+        if project.settings.android_platform_version:
+            options.set_capability(
+                "platformVersion",
+                project.settings.android_platform_version,
+            )
+
+        if project.settings.appWaitActivity:
+            options.set_capability("appWaitActivity", project.settings.appWaitActivity)
+
+        options.set_capability(
+            "app",
+            (
+                project.settings.app_path
+                if (project.settings.app_path.startswith('/') or config.runs_on_bstack)
+                else relative_from_root(project.settings.app_path)
+            ),
+        )
+
+        if config.runs_on_bstack:
+
+            options.set_capability(
+                'bstack:options',
+                {
                     "projectName": project.settings.android_project_name,
-                    "buildName": "browserstack-build-1",
-                    "sessionName": "Android tests",
+                    "buildName": "browserstack-build-remote",
+                    "sessionName": "Android tests Remote",
                     "userName": config.user_name,
                     "accessKey": config.access_key,
                 },
-            }
-        )
+            )
     elif project.settings.platform_name == 'ios':
         options = XCUITestOptions().load_capabilities(
             {
@@ -65,7 +89,7 @@ def mobile_management():
 
     session_id = browser.driver.session_id
 
-    with allure.step("Tear down session"):
+    with allure.step(f"Tear down session {session_id}"):
         browser.quit()
-
-    attach_video(session_id)
+    if config.runs_on_bstack:
+        attach_video(session_id)
